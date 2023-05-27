ae94454715bfd18d8d619ae045e322f4db565a17
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-27 01:15:16 -0500

    Use a getter to return the user-friendly persons

--- src/app.ts
+++ src/app.ts
@@ -174,6 +174,13 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private get persons(): string {
+    if (this.project.people === 1) {
+      return '1 person';
+    }
+    return `${this.project.people} persons`;
+  }
+
   constructor(hostId: string, private project: Project) {
     super('single-project', hostId, 'afterbegin', project.id);
 
@@ -184,8 +191,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent(): void {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent =
-      this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned.';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
